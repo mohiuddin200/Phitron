4d76ffa2b79f4ab98c82923636e4dd46b540d559
Mohiuddin <mohiuddin.niddu@gmail.com>
2025-09-01 00:26:14 +0600

    practice C code

---
+++ Module 3/i.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+  int A;
+  int B;
+
+  scanf("%d %d", &A, &B);
+
+  if (A >= B)
+  {
+    printf("Yes\n");
+  }
+
+  else
+  {
+    printf("No\n");
+  }
+  return 0;
+}
